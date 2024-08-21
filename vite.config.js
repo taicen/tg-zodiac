c9931b51a13d6ac1ce1c156b259b1c618a23d97a
Alexandr Trachuk <vip-tai@ya.ru>
2024-08-21 23:50:13 +0500

    build

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,16 @@ import fs from "fs";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 443,
-    host: "0.0.0.0",
-    hmr: {
-      host: "tg-zodiac-app.local",
-      port: 443,
-    },
-    https: {
-      key: fs.readFileSync("./.cert/localhost-key.pem"),
-      cert: fs.readFileSync("./.cert/localhost.pem"),
-    },
-  },
+  // server: {
+  //   port: 443,
+  //   host: "0.0.0.0",
+  //   hmr: {
+  //     host: "tg-zodiac-app.local",
+  //     port: 443,
+  //   },
+  //   https: {
+  //     key: fs.readFileSync("./.cert/localhost-key.pem"),
+  //     cert: fs.readFileSync("./.cert/localhost.pem"),
+  //   },
+  // },
 });
